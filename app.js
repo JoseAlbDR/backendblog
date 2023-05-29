294f70dc46f020df117134a41686f26a3fabe780
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-05-30 01:49:51 +0200

    Generate post page content dinamically based on id

--- app.js
+++ app.js
@@ -16,8 +16,12 @@ const bgImg = "home-bg.jpg";
 const post = {
   bgImg: "home-bg.jpg",
 };
+let id = 0;
 
-app.get;
+app.post("/", (req, res) => {
+  id = req.body.id;
+  res.redirect("/post");
+});
 
 // Route to root page (index)
 app.get("/", (req, res) => {
@@ -26,27 +30,45 @@ app.get("/", (req, res) => {
   res.render("home", {
     bgImg: "home-bg.jpg",
     postData: postData,
+    title: "Blog J.Alberto Delgado",
+    subtitle: "Aquí ire dejando dichas mis cositas.",
   });
 });
 
 // Route to about page
 app.get("/about", (req, res) => {
-  res.render("about", post);
+  res.render("about", {
+    bgImg: "about-bg.jpg",
+    title: "Sobre Mí.",
+    subtitle: "Algunas cosillas sobre mi",
+  });
 });
 
 // Route to contact page
 app.get("/contact", (req, res) => {
-  res.render("contact", { bgImg: "home-bg.jpg" });
+  res.render("contact", {
+    bgImg: "contact-bg.jpg",
+    title: "Contacta conmigo.",
+    subtitle:
+      "Rellena el formulario y me pondré en contacto contigo lo antes posible.",
+  });
 });
 
 // Route to post page
 app.get("/post", (req, res) => {
-  res.render("post", { bgImg: "home-bg.jpg" });
+  res.render("post", {
+    bgImg: postData[id].bgImg,
+    postData: postData,
+    title: postData[id].postTitle,
+    subtitle: postData[id].postSubtitle,
+    id: id,
+  });
 });
 
 // Route to compose page
 app.get("/compose", (req, res) => {
   res.render(__dirname + "/views/compose");
 });
+
 app.use(express.static(__dirname + "/public"));
 app.listen(port, () => console.log(`Server running in port: ${port}`));
--- public/js/data.js
+++ public/js/data.js
@@ -1,10 +1,14 @@
 import { getDate } from "./utils.js";
-
+let id = 0;
 export const postData = [
   {
     postTitle: "Man must explore, and this is exploration at its greatest",
     postSubtitle: "Problems look mighty small from 150 miles up",
     postDate: getDate(),
+    id: id++,
+    author: "J.Alberto Delgado",
+    bgImg: "post-bg.jpg",
+    web: "https://www.jadelgadorobles.com",
     content: `<p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.</p>
     <p>Science cuts two ways, of course; its products can be used for both good and evil. But there's no turning back from science. The early warnings about technological dangers also come from science.</p>
     <p>What was most significant about the lunar voyage was not that man set foot on the Moon but that they set eye on the earth.</p>
