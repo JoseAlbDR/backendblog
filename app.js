ce31bc79e02d88ed4c67a03a575c7bc4b5ce9306
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-05-29 22:38:25 +0200

    Add ejs template and route to contact

--- app.js
+++ app.js
@@ -6,15 +6,19 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-// Get root page (index)
+// Route to root page (index)
 app.get("/", (req, res) => {
   res.render(__dirname + "/views/index");
 });
 
-// Get about page
+// Route to about page
 app.get("/about", (req, res) => {
   res.render(__dirname + "/views/about");
 });
 
+// Route to contact page
+app.get("/contact", (req, res) => {
+  res.render(__dirname + "/views/contact");
+});
 app.use(express.static(__dirname + "/public"));
 app.listen(port, () => console.log(`Server running in port: ${port}`));
