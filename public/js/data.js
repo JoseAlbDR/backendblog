62068aedf88cabf5ccfa2af3bc362a6ba5889c6f
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-02 23:37:54 +0200

    Refactor data objects

--- public/js/data.js
+++ public/js/data.js
@@ -26,7 +26,7 @@ export const postData = [];
 
 export const aboutData = {
   bgImg: "about-bg.jpg",
-  title: "Sobre Mí.",
+  postTitle: "Sobre Mí.",
   subtitle: "Algunas cosillas sobre mi",
   content:
     "¡Hola! Soy J. Alberto Delgado, un estudiante principiante de desarrollo web. Mi pasión es resolver problemas a través de la programación. Actualmente, me encuentro en un bootcamp de desarrollo web full stack, ampliando mis habilidades en front-end y back-end. Mi meta es crecer como profesional en el desarrollo web y aportar mi conocimiento a proyectos futuros.",
@@ -34,14 +34,14 @@ export const aboutData = {
 
 export const homeData = {
   bgImg: "home-bg.jpg",
-  title: blogData.blogOwner,
-  subtitle: "Aquí ire dejando dichas mis cositas.",
+  postTitle: blogData.blogOwner,
+  postSubtitle: "Aquí ire dejando dichas mis cositas.",
 };
 
 export const contactData = {
   bgImg: "contact-bg.jpg",
-  title: "Contacta conmigo.",
-  subtitle:
+  postTitle: "Contacta conmigo.",
+  postSubtitle:
     "A parte de a traves de mis redes sociales puedes usar el siguiente formulario.",
   content:
     "¿Quieres ponerte en contácto? No dudes en rellenar el siguiente formulario y me pondré en contácto contigo lo antes posible.",
@@ -49,7 +49,7 @@ export const contactData = {
 
 export const composeData = {
   bgImg: "compose-bg.jpg",
-  title: "Escribe algo.",
-  subtitle: "Si estás aqui es porque quieres escribir algo.",
+  postTitle: "Escribe algo.",
+  postSubtitle: "Si estás aqui es porque quieres escribir algo.",
   content: "Es hora de escribir!",
 };
